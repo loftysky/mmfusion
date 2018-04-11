baa60ffad51fbb1df9181a6c8e145889fa48e133
Mike Boers <mikeb@loftysky.com>
2018-04-11 15:01:52 -0400

    First commit.
    This is blind and untested.

---
+++ mmfusion/render.py
@@ -0,0 +1,56 @@
+from future import print_function
+
+import argparse
+import os
+import re
+import sys
+
+
+def render_main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--name')
+    parser.add_argument('-s', '--start', type=int)
+    parser.add_argument('-e', '--end', type=int)
+    parser.add_argument('-c', '--chunk', type=int, default=5)
+    parser.add_argument('script')
+    args = parser.parse_args()
+
+    if args.start is None and args.end is None:
+        comp = open(args.script).read()
+        m = re.match(r'RenderRange\s*=\s*\{\s*(\d+),\s*(\d+)\s*\}', comp)
+        if not m:
+            print("Could not parse RenderRange from {}.".format(args.script), file=sys.stderr)
+            exit(1)
+        args.start = int(m.group(1))
+        args.end = int(m.group(2))
+
+    elif args.start is None or args.end is None:
+        print("Cannot provide only one of --start or --end.", file=sys.stderr)
+        exit(2)
+
+    args.name = args.name or 'Fusion :: {}'.format(os.path.splitext(os.path.basename(args.script))[0])
+
+
+    from farmsoup.client import Client
+
+    client = Client()
+    job = client.job(
+        name=args.name,
+    ).setup_as_subprocess([
+        'xvfb-run', '-s' '-screen 0 640x480x24',
+        '/opt/BlackmagicDesign/FusionRenderNode9/FusionRenderNode',
+        args.script,
+        '-render',
+        '-start', '@F',
+        '-end', '@F_end',
+    ])
+    job.expand_via_rane('F={}-{}/{}'.format(args.start, args.end, args.chunk))
+
+    group = client.submit(
+        name=args.name,
+        jobs=[job],
+    )
+
+    print(group.id)
+    
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mmfusion',
+    version='0.1.dev0',
+    description="Lofty Sky\'s Fusion tools",
+    url='http://github.com/loftysky/mmfusion',
+    
+    packages=find_packages(exclude=['build*', 'tests*']),
+    include_package_data=True,
+    
+    author='Mike Boers',
+    author_email='mmfusion@mikeboers.com',
+    license='BSD-3',
+    
+    entry_points={
+        'console_scripts': '''
+        
+            mmfusion-submit-render = mmfusion.render:submit_main
+
+        ''',
+    },
+    
+)
